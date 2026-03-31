0d552e623d68c9f47921d2adfb2c0177ccd6f3bc
lucrecolon <lucrecolon1828@gmail.com>
2026-03-31 13:30:21 -0300

    fix: brand logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { useCart } from '../context/CartContext';
 import { useNavigate } from 'react-router-dom';
 import { useWishlist } from '../context/WishlistContext';
-import logo from '../assets/logo_TTL.jpg';
+import logo from '../assets/IMG_4561.png';
 
 export const Header = () => {
     const { cartItems, setCartOpen } = useCart();
